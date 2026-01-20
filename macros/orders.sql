ba90afe687e8151458462d13b3a61b1897239fb1
Pujitha Alla <poojithalla1@gmail.com>
2026-01-20 11:46:29 +0000

    Added macro models.

---
+++ macros/orders.sql
@@ -0,0 +1,11 @@
+{% macro customer_orders_agg(orders_relation) %}
+
+select
+    customer_id,
+    min(order_date) as first_order_date,
+    max(order_date) as most_recent_order_date,
+    count(order_id) as number_of_orders
+from {{ orders_relation }}
+group by customer_id
+
+{% endmacro %}
